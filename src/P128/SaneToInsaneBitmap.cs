3650db788105e67ef842cb888b6900e35a0bd31d
Samma - PixelStudio <81291107+Samma2009@users.noreply.github.com>
2024-03-07 17:12:54 +0100

    browser
    change font

---
+++ src/P128/SaneToInsaneBitmap.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxima128.P128
+{
+    public class BitmapConverter
+    {
+
+        public static Mirage.GraphicsKit.Canvas CGSTOMIRRAGE(Cosmos.System.Graphics.Bitmap CGSImage)
+        {
+
+            Mirage.GraphicsKit.Canvas MBMP = new((ushort)CGSImage.Width, (ushort)CGSImage.Height);
+            for (int x = 0; x < CGSImage.Width; x++)
+            {
+                for (int y = 0; y < CGSImage.Height; y++)
+                {
+
+                    MBMP[x, y] = new Mirage.GraphicsKit.Color((uint)CGSImage.RawData[x + (y * CGSImage.Width)]);
+
+                }
+            }
+
+            return MBMP;
+
+        }
+
+    }
+}
